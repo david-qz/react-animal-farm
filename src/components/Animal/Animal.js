29a3647c818282fc8b560fb8b7a439d77ca839d8
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 13:34:03 -0700

    Implement Animal rendering

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,17 @@
+import './Animal.css';
+
 export default function Animal({ animal }) {
-  return <p>{animal.name}</p>;
+  return (
+    <div
+      className='animal'
+      style={{
+        top: animal.top,
+        left: animal.left
+      }}
+    >
+      <img alt={animal.name} src={`/animals/${animal.type}.svg`} />
+      <span className='name'>{animal.name}</span>
+      <span>{animal.says}</span>
+    </div>
+  );
 }
