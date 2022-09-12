28116af0cc5ddf2f8ce0b6b5e6adb8850681d643
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-12 12:43:09 -0700

    Render animal components into Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import Animal from '../Animal/Animal';
+import { animals } from '../../data';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return <main style={{ backgroundImage: `url(${background})` }}>
+    {animals.map(animal => <Animal key={animal.name} animal={animal}/>)}
+  </main>;
 }
